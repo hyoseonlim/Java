c41b1ba01b2b2661ada3d86ee8a2a9c8fe2b570b
Hyo <lovellehyo@gmail.com>
2025-03-21 13:46:33 +0900

    3주차 실습문제 풀이 수정 (2차원 배열)

--- src/lecture03/W3_5.java
+++ src/lecture03/W3_5.java
@@ -28,3 +28,11 @@ public class W3_5 {
 		scanner.close();
 	}
 }
+
+/*
+ * 정수 나누기 0: ArithmeticException null 레퍼런스 참조: NullPointerException 타입 변환 불가:
+ * ClassCastException 메모리 부족: OutOfMemoryError 배열의 범위를 벗어난 접근:
+ * ArrayIndexOutOfBoundsException 잘못된 인자 전달 시: IllegalArgumentException 입출력 동작
+ * 실패 또는 인터럽트 시: IOException 문자열이 나타내는 숫자와 일치하지 않는 타입의 숫자 변환 시:
+ * NumberFormatException Scanner의 nextInt() 호출했으나 정수 아니면: InputMismatchException
+ */
